e5ebdc0ce9f1d3bb446bf73a53c585225951b9dc
Harry Coultas Blum <l@baz.codes>
2021-02-05 13:26:46 +0000

    Checked running on a rig

--- scripts/setup-machine-scripts/template_script.sh
+++ scripts/setup-machine-scripts/template_script.sh
@@ -11,7 +11,7 @@ set -e
 # Use sudo if we aren't root when we need to
 function s {
     if [ `which sudo` ]; then
-        $@
+        sudo $@
     else
         $@
     fi
@@ -24,18 +24,28 @@ echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 s ln -fs /usr/share/zoneinfo/Europe/London /etc/localtime
 
 s apt update
-s apt install apt-transport-https software-properties-common -y --fix-missing
+s apt install apt-transport-https software-properties-common python3-apt -y --fix-missing
 s add-apt-repository ppa:apt-fast/stable -y
 s apt install -y apt-fast
 
 # Setup apt-fast
-echo debconf apt-fast/maxdownloads string 20 | debconf-set-selections
-echo debconf apt-fast/dlflag boolean false | debconf-set-selections
-echo debconf apt-fast/aptmanager string apt | debconf-set-selections
+s echo debconf apt-fast/maxdownloads string 20 | s debconf-set-selections
+s echo debconf apt-fast/dlflag boolean false | s debconf-set-selections
+s echo debconf apt-fast/aptmanager string apt | s debconf-set-selections
 
-s apt-fast install -y git curl fzf vim-gtk ncdu htop ripgrep build-essential tmux python3-pip python3.8 python3-apt
+s apt-fast install -y git curl vim-gtk ncdu htop build-essential tmux python3-pip python3.8 python3-apt
 
-s update-alternatives --install /usr/bin/python python /usr/bin/python3.8 0
+# FZF
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# Ripgrep
+
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb
+s dpkg -i ripgrep_12.1.1_amd64.deb
+rm ripgrep_12.1.1_amd64
+
+# s update-alternatives --install /usr/bin/python python /usr/bin/python3.8 0
 
 # Install Node
 curl https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
