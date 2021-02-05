ad0cbbc1f36ff09b566bf5ca0537f3ec1229f612
Harry Coultas Blum <l@baz.codes>
2021-02-05 13:01:33 +0000

    reverted back to password ssh

--- scripts/setup-machine-scripts/make_script.sh
+++ scripts/setup-machine-scripts/make_script.sh
@@ -1,5 +1,15 @@
 #/bin/bash
-SSH_KEY=$(cat ~/.ssh/id_rsa)
-SSH_KEY_PUB=$(cat ~/.ssh/id_rsa.pub)
+IFS=$'\n'
+mkdir -p ssh
+cp ~/.ssh/id_rsa ssh
+cp ~/.ssh/id_rsa.pub ssh 
+cp ~/.ssh/config ssh
+ssh-keygen -p -f ssh/id_rsa
+SSH_KEY=$(cat ssh/id_rsa)
+SSH_KEY_PUB=$(cat ssh/id_rsa.pub)
+
 python ./create_tools_script.py > install.sh
+
+# Delete the folder we generated
+rm -rf ssh
 chmod +x install.sh
