b1dbe68061e7f1faf06fe21c445092cd0981e26f
Harry Coultas Blum <l@baz.codes>
2021-02-16 17:02:32 +0000

    Added script to change author

---
+++ scripts/change_git_author.sh
@@ -0,0 +1,15 @@
+git filter-branch -f --env-filter '
+OLD_EMAIL="himion0@gmail.com"
+CORRECT_NAME="Harry Coultas Blum"
+CORRECT_EMAIL="l@baz.codes"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
